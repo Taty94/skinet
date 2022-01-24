99b3c24a7466be5c7676196efbf7538511167541
Taty94 <tatiana94montenegro@gmail.com>
2022-01-24 12:03:14 -0500

    End of section 7

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';//necesario para ngx-bootstrap
+
+@NgModule({
+  //declaramos nuestros componenetes
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
